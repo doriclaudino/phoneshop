e08ccf22378a691d73e11e76ac2f440478359b59
dori claudino <dori.claudino@gmail.com>
2018-07-17 13:08:12 -0400

    ask authentication for brand and brand details

--- phoneshop/settings.py
+++ phoneshop/settings.py
@@ -68,6 +68,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 
