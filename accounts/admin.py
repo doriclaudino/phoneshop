a8774f5b954384227be80bd923ed90b361ebb165
dori claudino <dori.claudino@gmail.com>
2018-07-21 11:22:53 -0400

    new user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from accounts.models import User
+from django.contrib.auth import models, forms
+from django.contrib import auth
 
-# Register your models here.
+# admin.site.register(models.Group)
+
+
+class UserCreateForm(forms.UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', )
+
+
+class UserAdmin(auth.admin.UserAdmin):
+    add_form = UserCreateForm
+    prepopulated_fields = {'username': ('first_name', 'last_name', )}
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', ),
+        }),
+    )
+
+
+# Re-register UserAdmin
+admin.site.register(User, UserAdmin)
